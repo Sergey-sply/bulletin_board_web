92c61ae022efb28cad00ba3da18b6633827efb61
Sergey-sply <sergeysedov12@gmail.com>
2023-08-02 23:14:16 +0300

    add rubric, add rubric model, changed .html files, changed view

--- bboard/urls.py
+++ bboard/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, rubric_bbs
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='index'),
+
+    path('<int:rubric_id>/', rubric_bbs, name='rubric_bbs'),
 ]
+
